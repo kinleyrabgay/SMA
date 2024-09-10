31a4018f3dbe60f474032626ba50c3ce942bd241
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-10 15:58:21 +0600

    fix(app): config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    // Cache 30 seconds
+    staleTimes: {
+      dynamic: 30,
+    }
+  }
+};
 
 export default nextConfig;
