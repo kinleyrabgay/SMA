0be8943c5aa830605ef98c1be63dc3774b461772
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-10 19:00:07 +0600

    fix(app): fixed api routes

--- src/app/(main)/MenuBar.tsx
+++ src/app/(main)/MenuBar.tsx
@@ -16,15 +16,15 @@ export default async function MenuBar({ className }: MenuBarProps) {
 
   if (!user) return null;
 
-  const [unreadNotificationsCount, unreadMessagesCount] = await Promise.all([
-    prisma.notification.count({
-      where: {
-        recipientId: user.id,
-        read: false,
-      },
-    }),
-    (await streamServerClient.getUnreadCount(user.id)).total_unread_count,
-  ]);
+  // const [unreadNotificationsCount, unreadMessagesCount] = await Promise.all([
+  //   prisma.notification.count({
+  //     where: {
+  //       recipientId: user.id,
+  //       read: false,
+  //     },
+  //   }),
+  //   (await streamServerClient.getUnreadCount(user.id)).total_unread_count,
+  // ]);
 
   return (
     <div className={className}>
@@ -39,10 +39,10 @@ export default async function MenuBar({ className }: MenuBarProps) {
           <span className='hidden lg:inline'>Home</span>
         </Link>
       </Button>
-      <NotificationsButton
+      {/* <NotificationsButton
         initialState={{ unreadCount: unreadNotificationsCount }}
-      />
-      <MessagesButton initialState={{ unreadCount: unreadMessagesCount }} />
+      /> */}
+      {/* <MessagesButton initialState={{ unreadCount: unreadMessagesCount }} /> */}
       <Button
         variant='ghost'
         className='flex items-center justify-start gap-3'
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,7 +2,7 @@ import PostEditor from '@/components/posts/editor/PostEditor';
 import TrendsSidebar from '@/components/TrendsSidebar';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 // import FollowingFeed from './FollowingFeed';
-// import ForYouFeed from './ForYouFeed';
+import ForYouFeed from './ForYouFeed';
 
 export default function Home() {
   return (
@@ -14,11 +14,13 @@ export default function Home() {
             <TabsTrigger value='for-you'>For you</TabsTrigger>
             <TabsTrigger value='following'>Following</TabsTrigger>
           </TabsList>
-          <TabsContent value='for-you'>{/* <ForYouFeed /> */}</TabsContent>
+          <TabsContent value='for-you'>
+            <ForYouFeed />
+          </TabsContent>
           <TabsContent value='following'>{/* <FollowingFeed /> */}</TabsContent>
         </Tabs>
       </div>
-      <TrendsSidebar />
+      {/* <TrendsSidebar /> */}
     </main>
   );
 }
